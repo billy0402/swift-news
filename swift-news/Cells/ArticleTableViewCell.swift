0c19c8482bd288561ba46b3dd722e0116a9f9af6
Billy Huang <10646003@ntub.edu.tw>
2020-03-14 14:49:19 +0800

    Creating Custom Self Sizing Cells

---
+++ swift-news/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  swift-news
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
