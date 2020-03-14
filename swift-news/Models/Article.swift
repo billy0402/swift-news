16a68f0e3e474bc8047a8be5214a1cc6885fbae2
Billy Huang <10646003@ntub.edu.tw>
2020-03-14 14:28:39 +0800

    Creating and Decoding JSON to Models

---
+++ swift-news/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  swift-news
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+}
---
+++ swift-news/Models/ArticleList.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleList.swift
+//  swift-news
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleList: Decodable {
+    let articles: [Article]
+}
