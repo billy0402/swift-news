f6454ccdfe9abd4c3c434d738c1ab005b472865e
Billy Huang <10646003@ntub.edu.tw>
2020-03-14 11:57:39 +0800

    Initial Commit

---
+++ swift-news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-news
+//
+//  Created by User on 2020/3/14.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
